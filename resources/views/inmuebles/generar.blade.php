71ce01855facf372060579729a10209d3fd2057f
jgonmor <antonio@hawkins.es>
2024-10-17 15:39:24 +0200

    change pdf mercury

--- resources/views/inmuebles/generar.blade.php
+++ resources/views/inmuebles/generar.blade.php
@@ -9,169 +9,145 @@
 
         .full-width-image {
             width: 100%;
-            height: auto; /* Mantiene la proporción de la imagen */
-            max-height: 300px; /* Limita la altura máxima */
+            height: auto;
+            max-height: 400px; /* Limita la altura máxima */
+            margin-bottom: 20px;
         }
 
-        .dynamic-width-image {
-            width: auto; /* Mantiene la proporción de la imagen */
-            max-height: 300px; /* Limita la altura máxima */
-            float: left;
+        .info-container {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        .info-container table {
+            width: 100%;
+            table-layout: fixed;
+            border-collapse: collapse;
+        }
+
+        .info-container td {
+            vertical-align: top;
+        }
+
+        .info-container .left,
+        .info-container .right {
+            width: 50%;
+            text-align: left; /* Cambié de 'start' a 'left' para compatibilidad con PDF */
+            vertical-align: top;
+        }
+
+        .info-container strong {
+            font-weight: bold;
+        }
+
+        .description {
+            margin-top: 10px;
+            border-top: 1px solid #ccc;
+            border-bottom: 1px solid #ccc;
+            color: #333;
+            padding: 20px;
+            margin-bottom: 40px;
+        }
+
+        .gallery {
+            margin-top: 20px;
+        }
+
+        .gallery img {
+            width: 48%; /* Ajuste para dos imágenes por fila */
+            height: 200px; /* Altura fija para mantener el cuadrado */
+            margin-bottom: 10px;
+            float: left; /* Hace que las imágenes se alineen lado a lado */
+            margin-right: 4%; /* Espacio entre columnas */
+            border: 1px solid #ddd;
+            object-fit: cover; /* Asegura que la imagen se ajuste al tamaño sin distorsionarse */
+        }
+
+        /* Forzamos que la siguiente fila de imágenes comience en una nueva fila */
+        .gallery img:nth-child(2n+1) {
+            clear: both;
         }
 
         .clear {
             clear: both;
         }
 
-        .info-container {
-            width: 100%;
-            margin-top: 20px;
-        }
-
-        .info-table {
-            width: 100%;
-            table-layout: fixed;
-            border-collapse: collapse;
-        }
-
-        .info-row {
-            display: table-row;
-        }
-
-        .info-item {
-            display: table-cell;
-            font-weight: bold;
-            text-align: left;
-            padding: 6px 0px;
-        }
-
-        .info-item:nth-child(2) {
-            text-align: center;
-        }
-
-        .info-item:last-child {
-            text-align: right;
-        }
-
-        .features-container {
-            margin-top: 20px;
-            width: 100%;
-        }
-
-        .feature-item {
-            display: inline-block;
-            border: 1px solid #000;
-            text-align: center;
-            box-sizing: border-box;
-            padding: 5px 0px;
-        }
-
-        .feature-item.large {
-            width: 24.2%;
-        }
-
-        .feature-item.small {
-            margin-top: 5px;
-            width: 49.2%;
-        }
-
-        .logo {
-            width: 100%;
-            text-align: center;
-            margin-bottom: 20px;
-            /* Espacio adicional arriba y abajo del logo */
-        }
-        .head-title{
-            margin-bottom: 20px;
-            margin-top: 20px;
-        }
     </style>
 </head>
 
-<body class="bg-dark" style="background:black;">
-    @if($logo != null)
-    <div class="logo">
-        <img src="{{ public_path('assets/logo.png') }}" alt="Logo de la empresa" style="width: 150px; background:white;">
+<body>
+
+    <!-- Imagen principal -->
+    @if(count($galeria) > 0)
+    <div>
+        <img src="{{ public_path($galeria[1]) }}" class="full-width-image" alt="Imagen principal del inmueble"> <!-- No es la primera imagen -->
     </div>
     @endif
-
-    <div class="head-title">
-        <h1 class="text-center m-0 p-0" style="text-align:center; margin:0px; padding:0px; color:white; font-size: 2.5rem"> @if($title != null) {{$title}}  @else  @if($inmueble->disponibilidad == 'Alquiler') RENTAL OPORTUNITY @else INVESTMENT OPORTUNITY  @endif @endif</h1>
+    <div>
+        <p style="text-align: right; font-size: 1rem; margin-top: 20px; margin-bottom: 20px;">
+            Ref: {{ $inmueble->reference_number }}
+        </p>
     </div>
-    <div class="">
-        @if(count($galeria) > 0)
-        <div style="max-height:300px; display:block; position:relative; width: 100%;">
-            <img src="{{ public_path(reset($galeria)) }}" class="full-width-image" alt="Imagen del inmueble">
-        </div>
-        @else
-        <p>There are no images available for this property.</p>
-        @endif
-
-        <div class="features-container">
-            <table style="width:100%; font-size: 2rem; padding:20px;">
-                <tr>
-                    <td class=" text-white" style="color:white ; width: 100%;"><strong>REF:</strong> {{ $inmueble->reference_number }}</td>
-                    <td class=" text-white" style="color:white; width:100%;"> <strong>PRICE:</strong>
-                        @if($inmueble->disponibilidad == 'Venta')
-                        {{ $inmueble->precio_venta }}
-                        @else
-                        @if($inmueble->daily_rental_price)
-                        {{ $inmueble->daily_rental_price }}
+    <div class="info-container">
+        <table>
+            <tr>
+                <td class="left" style="padding-right: 10px; border-right: 1px solid #ccc;">
+                    @if($inmueble->disponibilidad == 'Alquiler')
+                        @if($inmueble->alquiler_semana)
+                            <strong>{{ $inmueble->alquiler_semana }}</strong>
                         @elseif($inmueble->alquiler_mes)
-                        {{ $inmueble->alquiler_mes }}
-                        @else
-                        {{ $inmueble->alquiler_semana }}
+                            <strong>{{ $inmueble->alquiler_mes }} </strong>
+                        @else   
+                            <strong>{{ $inmueble->daily_rental_price ?? '-' }}</strong><br>
                         @endif
-                        @endif
-                </tr>
-                @if( $inmueble->m2_construidos)
-                <tr>
-                    <td class=" text-white" style="color:white ; width: 100%;"><strong>SIZE:</strong> {{ $inmueble->m2_construidos }}</td>
-                    <td class=" text-white" style="color:white ; width: 100%;"></td>
-                </tr>
-                @endif
-                @if( $inmueble->garaje)
-                <tr>
-                    <td class=" text-white" style="color:white ; width: 100%;"><strong>Garage:</strong> {{ $inmueble->garaje ? 'Yes' : 'No' }}</td>
-                    <td class=" text-white" style="color:white ; width: 100%;"></td>
-                </tr>
-                @endif
-                @if( $inmueble->piscina)
-                <tr>
-                    <td class=" text-white" style="color:white ; width: 100%;"><strong>Pool:</strong> {{ $inmueble->piscina ? 'Yes' : 'No' }}</td>
-                    <td class=" text-white" style="color:white ; width: 100%;"></td>
-                </tr>
-                @endif
-            </table>
+                    @else
+                        <strong>{{ $inmueble->precio_venta ?? '-' }}</strong>
+                    @endif
+                    <strong>Location:</strong> {{ $inmueble->direccion }}, {{ $inmueble->localidad }}<br>
+                    <strong>Property type:</strong> {{ $inmueble->tipo_inmueble }}<br>
+                    <strong>Zone:</strong> {{ $inmueble->localidad }}
 
-        </div>
-        <div style="margin-top: 10px; border-top: 1px solid #ccc; border-bottom: 1px solid #ccc; color:white">
-            <p style="margin-top: 10px; color:white; padding: 20px;"> {{ $inmueble->descripcion }}</p>
-        </div>
-        @if(count($galeria) > 0)
+                </td>
+                <td class="right" style="padding-left: 10px;">
+                    <strong>Area:</strong> {{ $inmueble->m2 ?? '-' }} m²<br>
+                    <strong>Built area:</strong> {{ $inmueble->m2_construidos ?? '-' }} m²<br>
+                    <strong>Rooms:</strong> {{ $inmueble->habitaciones ?? '-' }}<br>
+                    <strong>Bathrooms:</strong> {{ $inmueble->banos }}<br>
+                </td>
+            </tr>
+        </table>
+    </div>
+    
 
-            @php
-            $imagenesMostradas = 0;
-            @endphp
+    <!-- Descripción -->
+    <div class="description">
+        <strong style="width: 100%; text-align: center; display:block;">DESCRIPTION</strong>
+        <p style="white-space: pre-wrap;">{{ $inmueble->descripcion }}</p>
+    </div>
 
-            @foreach($galeria as $key => $imagen)
-                @if ($imagenesMostradas != 0 || $imagenesMostradas == 5)
-                    <div style="max-height:300px; display:block; position:relative; width: 97%; padding: 10px;">
-                        <img style="margin: 0 auto;" src="{{ public_path($imagen) }}" class="full-width-image" alt="Imagen del inmueble">
-                    </div>
-                @endif
-
-                @php
-                $imagenesMostradas++;
-                @endphp
-
-                @if ($imagenesMostradas >= 5)
-                    @break
-                @endif
-            @endforeach
-        @endif
+    <!-- Galería de fotos en dos columnas con tamaño uniforme -->
+    @if(count($galeria) > 0)
+    <div style="text-align: center;">
+        <strong >PHOTO REPORT</strong>
 
     </div>
+    <div class="gallery">
+
+        @foreach($galeria as $key => $imagen)
+        @if ($key > 0) <!-- Evitar mostrar la imagen principal -->
+        <img src="{{ public_path($imagen) }}" alt="Imagen del inmueble">
+
+        @if (($loop->index - 1) % 2 == 1) <!-- Limpiar después de cada dos imágenes -->
+        <div class="clear"></div>
+        @endif
+
+        @endif
+        @endforeach
+    </div>
+@else
+    <p>No hay imágenes disponibles para esta propiedad.</p>
+@endif
+
 </body>
 
 </html>
